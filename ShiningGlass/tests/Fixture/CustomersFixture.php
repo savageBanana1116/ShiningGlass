b3da021eaf267d9c819dab9d1ce1ab5601f33689
Cybertas <chris20419@gmail.com>
2022-04-02 14:22:19 +1100

    bake testing with customers entity

---
+++ ShiningGlass/tests/Fixture/CustomersFixture.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * CustomersFixture
+ */
+class CustomersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'first_name' => 'Lorem ipsum dolor sit amet',
+                'last_name' => 'Lorem ipsum dolor sit amet',
+                'email' => 'Lorem ipsum dolor sit amet',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'phone' => 1,
+                'id' => 1,
+            ],
+        ];
+        parent::init();
+    }
+}
