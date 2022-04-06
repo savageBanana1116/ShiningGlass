4d2e5f9124aaf83558de3c37611b0666d3215e6f
Khargan <kpow0010@student.monash.edu>
2022-04-06 12:54:50 +1000

    Admin Template Fix Javascript - Sidebar arrows to be fixed

--- ShiningGlass/templates/AdminPortal/index.php
+++ ShiningGlass/templates/AdminPortal/index.php
@@ -12,14 +12,14 @@
     <title>Shining Glass Admin</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+<!--    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">-->
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <?= $this->Html->css('sb-admin-2.min.css') ?>
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+<!--    <link href="css/sb-admin-2.min.css" rel="stylesheet">-->
 
 </head>
 
@@ -745,7 +745,7 @@
 <!--    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>-->
 
     <!-- Core plugin JavaScript-->
-    <?= $this->Html->script('/vendor/jquery-easing/jquery.easing.min.js'); ?>
+    <?= $this->Html->script('vendor/jquery-easing/jquery.easing.min.js'); ?>
 <!--    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>-->
 
     <!-- Custom scripts for all pages-->
@@ -757,9 +757,9 @@
 <!--    <script src="vendor/chart.js/Chart.min.js"></script>-->
 
     <!-- Page level custom scripts -->
-    <?= $this->Html->script('/demo/chart-area-demo.js'); ?>
+    <?= $this->Html->script('demo/chart-area-demo.js'); ?>
 <!--    <script src="js/demo/chart-area-demo.js"></script>-->
-    <?= $this->Html->script('/demo/chart-pie-demo.js'); ?>
+    <?= $this->Html->script('demo/chart-pie-demo.js'); ?>
 <!--    <script src="js/demo/chart-pie-demo.js"></script>-->
 
 </body>
