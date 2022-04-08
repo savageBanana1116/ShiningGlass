916f45a1ae107117164e7efa53be342f786058ce
MingJing Liu <mliu0044@student.monash.edu>
2022-04-09 02:07:15 +0800

    Landing page modified refer to the wireframe. -Carousel added -Contact details on footer -one sentence div -css modified

---
+++ ShiningGlass/templates/element/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer py-4">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-4 text-lg-start">Copyright &copy; Shining Glass</div>
+            <div class="col-lg-4 text-lg-middle">
+                <a class="link-dark text-decoration-none me-3" href="#!">Email: Lisa@shiningglass.com.au</a>
+                <a class="link-dark text-decoration-none" href="#!">Mobile: 04451267432</a>
+            </div>
+        </div>
+    </div>
+</footer>
