d9bb77a3a3cdbc94fdd6cdc734cc8aba68c38d90
hkil0001 <hkil0001@student.monash.edu>
2022-04-09 00:21:01 +1000

    09/04 (12:25am)
    - Updated UI and layout of the admin portal

--- ShiningGlass/templates/element/nav.php
+++ ShiningGlass/templates/element/nav.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div class="container">
         <a class="navbar-brand" href="#page-top">Shining Glass</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars ms-1"></i>
         </button>
@@ -12,7 +13,6 @@
                 <li class="nav-item"><a class="nav-link" href="blog">Blog</a></li>
                 <li class="nav-item"><a class="nav-link" href="contact">Contact</a></li>
                 <li class="nav-item"><a class="nav-link" href="adminportal#">Admin Portal</a></li>
-
             </ul>
         </div>
     </div>
