ed2cdfcabca7e29e671733bff6f5697e5fd51679
MingJing Liu <mliu0044@student.monash.edu>
2022-04-10 19:44:48 +0800

    footer updated

--- ShiningGlass/templates/element/footer.php
+++ ShiningGlass/templates/element/footer.php
@@ -1,4 +1,12 @@
-<footer class="footer py-4">
+<style>
+    #ft-1{
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        background-color: gray;
+    }
+</style>
+<footer class="footer py-4" id="ft-1">
     <div class="container">
         <div class="row align-items-center">
             <div class="col-lg-4 text-lg-start">Copyright &copy; Shining Glass</div>
