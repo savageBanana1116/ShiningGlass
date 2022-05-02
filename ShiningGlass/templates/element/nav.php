267e791f797d13179dfe8b9b05cf8bb2e9d38b75
MingJing Liu <mliu0044@student.monash.edu>
2022-05-02 20:49:12 +0800

    Fixed nav bar for local development

--- ShiningGlass/templates/element/nav.php
+++ ShiningGlass/templates/element/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="index.php">Shining Glass</a>
+        <a class="navbar-brand" href="/team18-app_fit3047/ShiningGlass/index.php">Shining Glass</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
                 aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
@@ -8,11 +8,38 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                <li class="nav-item"><a class="nav-link" href="about">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="Gallery">Gallery</a></li>
-                <li class="nav-item"><a class="nav-link" href="Blog">Blog</a></li>
-                <li class="nav-item"><a class="nav-link" href="AdminPortal">Admin Portal</a></li>
+                <li class="nav-item"><a class="nav-link" href="/team18-app_fit3047/ShiningGlass/about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="/team18-app_fit3047/ShiningGlass/Gallery">Gallery</a></li>
+                <li class="nav-item"><a class="nav-link" href="/team18-app_fit3047/ShiningGlass/Blog">Blog</a></li>
+                <li class="nav-item"><a class="nav-link" href="/team18-app_fit3047/ShiningGlass/AdminPortal">Admin Portal</a></li>
             </ul>
         </div>
     </div>
 </nav>
+
+
+
+
+
+<!--Nav code for cPanel-->
+
+
+<!--<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">-->
+<!--    <div class="container">-->
+<!--        <a class="navbar-brand" href="/index.php">Shining Glass</a>-->
+<!--        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"-->
+<!--                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--            Menu-->
+<!--            <i class="fas fa-bars ms-1"></i>-->
+<!--        </button>-->
+<!--        <div class="collapse navbar-collapse" id="navbarResponsive">-->
+<!--            <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">-->
+<!--                <li class="nav-item"><a class="nav-link" href="/about">About</a></li>-->
+<!--                <li class="nav-item"><a class="nav-link" href="/Gallery">Gallery</a></li>-->
+<!--                <li class="nav-item"><a class="nav-link" href="/Blog">Blog</a></li>-->
+<!--                <li class="nav-item"><a class="nav-link" href="/AdminPortal">Admin Portal</a></li>-->
+<!--            </ul>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</nav>-->
+
