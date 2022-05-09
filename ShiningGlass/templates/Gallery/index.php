f28f2b77705b447ee5c44c5848a4beea7f962674
hkil0001 <hkil0001@student.monash.edu>
2022-05-09 18:04:32 +1000

    Gallery Page Form Implementation: - Correct fields are present - Validation and styling needs to be finalised

--- ShiningGlass/templates/Gallery/index.php
+++ ShiningGlass/templates/Gallery/index.php
@@ -4,7 +4,6 @@
  * @var \App\Model\Entity\Artwork $artwork
  * @var \Cake\Collection\CollectionInterface|string[] $categories
  *  * @var \App\Model\Entity\Artwork[]|\Cake\Collection\CollectionInterface $artworks
-
  */
 ?>
 <!DOCTYPE html>
@@ -41,129 +40,146 @@
 <!---->
 <!--        <option > --Select a category to filter--</option>-->
 <!--        --><?php //foreach ($result as $categories) { ?>
-<!--        <option value="--><?php //$categories->category_id?><!--">--><?php //echo $categories->name?><!--</option>-->
+<!--        <option value="--><?php //$categories->category_id?><!--">-->
+<?php //echo $categories->name?><!--</option>-->
 <!---->
 <!--        --><?php //} ?>
 <!--        --><?php //$category_id = $_POST['category_id']?>
 <!--    </select>-->
 <div
-    style ="padding-top: 30px; background-color: #d5d8db ;font-size: 20px ;text-align: center ; padding-bottom: 30px ; font-weight: bold; width:100%;"> The Artworks of Sam Smith
+    style="padding-top: 30px; background-color: #d5d8db ;font-size: 20px ;text-align: center ; padding-bottom: 30px ; font-weight: bold; width:100%;">
+    The Artworks of Sam Smith
 </div>
- <div class="drop">
+<div class="drop">
 
 
-     <?= $this->Form->create(null,['type'=>'get'])?>
-     <?= $this->Form->control('categories_id', ['options' => $categories, 'class' => 'form-control']);;?>
-     <?= $this->Form->submit('Filter')?>
-     <?= $this->Form->end()?>
+    <?= $this->Form->create(null, ['type' => 'get']) ?>
+    <?= $this->Form->control('categories_id', ['options' => $categories, 'class' => 'form-control']);; ?>
+    <?= $this->Form->submit('Filter') ?>
+    <?= $this->Form->end() ?>
 
 
- </div>
+</div>
 
 <div class="container">
     <div class="row" style="margin-top: 60px">
         <?php $i = 1; ?>
         <?php foreach ($results as $artwork): ?>
-        <div class="col">
-            <div class="card card shadow mb-4">
-                <?= $this->Html->image($artwork->image, ['class' => 'card-img-top', 'width' => '400px', 'height' => '250px']) ?>
-                <div class="card-body" >
-                    <h5 class="card-title"><?= h($artwork->name) ?></h5>
-                    <p class="card-text">$<?= h($artwork->price) ?></p>
-                    <!--                    <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Read-->
-                    <!--                        More</a>-->
-                    <button type="button" class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#myModal-<?php echo $i; // Displaying the increment ?>">
-                        View More
-                    </button>
-                    <!-- Modal -->
-                    <div class="modal fade" id="myModal-<?php echo $i; // Displaying the increment ?>" tabindex="-1" role="dialog"
-                         aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                        <div class="modal-xl modal-dialog modal-dialog-centered" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h4 class="modal-title" id="exampleModalLongTitle"><?= h($artwork->name) ?></h4>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="row gx-4 gx-lg-5 align-items-center my-5">
-                                        <div class="col-lg-7"><?= $this->Html->image($artwork->image, ['style' => 'max-height: 900px; max-width: 400px']) ?>
-                                            <div style="max-width: 900px; margin-top: 15px";>
-                                                <?= $this->Html->image($artwork->image2, ['style' => 'max-height: 150px; max-width: 150px']) ?>
-                                                <?= $this->Html->image($artwork->image3, ['style' => 'max-height: 150px; max-width: 150px']) ?>
-                                                <?= $this->Html->image($artwork->image4, ['style' => 'max-height: 150px; max-width: 150px']) ?>
-                                                <?= $this->Html->image($artwork->image5, ['style' => 'max-height: 150px; max-width: 150px']) ?>
-                                        </div>
-                                        </div>
+            <div class="col">
+                <div class="card card shadow mb-4">
+                    <?= $this->Html->image($artwork->image, ['class' => 'card-img-top', 'width' => '400px', 'height' => '250px']) ?>
+                    <div class="card-body">
+                        <h5 class="card-title"><?= h($artwork->name) ?></h5>
+                        <p class="card-text">$<?= h($artwork->price) ?></p>
+                        <!--                    <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Read-->
+                        <!--                        More</a>-->
+                        <button type="button" class="btn btn-primary " data-bs-toggle="modal"
+                                data-bs-target="#myModal-<?php echo $i; // Displaying the increment ?>">
+                            View More
+                        </button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="myModal-<?php echo $i; // Displaying the increment ?>" tabindex="-1"
+                             role="dialog"
+                             aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                            <div class="modal-xl modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h4 class="modal-title" id="exampleModalLongTitle"><?= h($artwork->name) ?></h4>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                                            <div
+                                                class="col-lg-7"><?= $this->Html->image($artwork->image, ['style' => 'max-height: 900px; max-width: 400px']) ?>
+                                                <div style="max-width: 900px; margin-top: 15px">
+                                                    <?= $this->Html->image($artwork->image2, ['style' => 'max-height: 150px; max-width: 150px']) ?>
+                                                    <?= $this->Html->image($artwork->image3, ['style' => 'max-height: 150px; max-width: 150px']) ?>
+                                                    <?= $this->Html->image($artwork->image4, ['style' => 'max-height: 150px; max-width: 150px']) ?>
+                                                    <?= $this->Html->image($artwork->image5, ['style' => 'max-height: 150px; max-width: 150px']) ?>
+                                                </div>
+                                            </div>
 
 
-                                        <div class="col-lg-5">
-                                            <h1 class="font-weight-light"><?= h($artwork->name) ?></h1>
-                                            <h4>$<?= h($artwork->price) ?></h4>
-                                            <hr>
-<!--                                            <br>-->
-                                            <p style="text-align: left"><?= h($artwork->descriptions) ?></p>
-                                            <hr>
-<!--                                            <br>-->
-                                            <p style="text-align: left">Size: <?= h($artwork->size) ?> Cm Width and Height<br>
-                                                Item Weight: <?= h($artwork->weight) ?> Kg</p>
-                                            <hr>
-                                            <br>
+                                            <div class="col-lg-4">
+                                                <h1 class="font-weight-light"><?= h($artwork->name) ?></h1>
+                                                <h4>$<?= h($artwork->price) ?></h4>
+                                                <hr>
+                                                <!--                                            <br>-->
+                                                <p style="text-align: left"><?= h($artwork->descriptions) ?></p>
+                                                <hr>
+                                                <!--                                            <br>-->
+                                                <p style="text-align: left">Size: <?= h($artwork->size) ?> Cm Width and
+                                                    Height<br>
+                                                    Item Weight: <?= h($artwork->weight) ?> Kg</p>
+                                                <hr>
+                                                <br>
 
-                                            <script>
-                                                $(document).ready(function() {
-                                                    $("#formButton-<?php echo $i; // Displaying the increment ?>").click(function(){
-                                                        $("#form-<?php echo $i; // Displaying the increment ?>").toggle();
+                                                <script>
+                                                    $(document).ready(function () {
+                                                        $("#formButton-<?php echo $i; // Displaying the increment ?>").click(function () {
+                                                            $("#form-<?php echo $i; // Displaying the increment ?>").toggle();
+                                                        });
                                                     });
-                                                });
-                                            </script>
-                                            <button type="button" id="formButton-<?php echo $i; // Displaying the increment ?>">Open Order Form</button>
+                                                </script>
+                                                <button type="button"
+                                                        id="formButton-<?php echo $i; // Displaying the increment ?>">
+                                                    Open Order Form
+                                                </button>
 
-                                            <form id="form-<?php echo $i; // Displaying the increment ?>">
+                                                <form id="form-<?php echo $i; // Displaying the increment ?>">
                                                     <div class="form-row">
-                                                        <div class="form-group col-md-6">
-                                                            <label for="inputEmail4">Email</label>
-                                                            <input class="form-control" id="inputEmail4" placeholder="Email">
+                                                        <div class="form-group">
+                                                            <label for="inputFullName">Full name</label>
+                                                            <input class="form-control" id="inputFullName"
+                                                                   placeholder="Fullname">
                                                         </div>
-                                                        <div class="form-group col-md-6">
-                                                            <label for="inputPassword4">Password</label>
-                                                            <input class="form-control" id="inputPassword4" placeholder="Password">
+                                                        <div class="form-group">
+                                                            <label for="inputEmail">Email</label>
+                                                            <input type="text" class="form-control" id="inputEmail"
+                                                                   placeholder="Email">
                                                         </div>
                                                     </div>
                                                     <div class="form-group">
                                                         <label for="inputAddress">Address</label>
-                                                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label for="inputAddress2">Address 2</label>
-                                                        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                                                        <input class="form-control" id="inputAddress"
+                                                               placeholder="Address">
                                                     </div>
                                                     <div class="form-row">
-                                                        <div class="form-group col-md-6">
+                                                        <div class="form-group">
                                                             <label for="inputCity">City</label>
-                                                            <input type="text" class="form-control" id="inputCity">
+                                                            <input type="text" class="form-control" id="inputCity"
+                                                                   placeholder="City">
                                                         </div>
-                                                        <div class="form-group col-md-4">
+                                                        <div class="form-group">
                                                             <label for="inputState">State</label>
                                                             <select id="inputState" class="form-control">
-                                                                <option selected>Choose...</option>
-                                                                <option>...</option>
+                                                                <option>ACT</option>
+                                                                <option>NA</option>
+                                                                <option>VIC</option>
+                                                                <option>WA</option>
+                                                                <option>QLD</option>
+                                                                <option>SA</option>
+                                                                <option>TAS</option>
                                                             </select>
                                                         </div>
-                                                        <div class="form-group col-md-2">
+                                                        <div class="form-group">
                                                             <label for="inputZip">Zip</label>
-                                                            <input type="text" class="form-control" id="inputZip">
+                                                            <input type="number" class="form-control" id="inputZip">
                                                         </div>
                                                     </div>
                                                     <div class="form-group">
                                                         <div class="form-check">
-                                                            <input class="form-check-input" type="checkbox" id="gridCheck">
                                                             <label class="form-check-label" for="gridCheck">
-                                                                Check me out
+                                                                Confirm Details
                                                             </label>
+                                                            <input class="form-check-input" type="checkbox"
+                                                                   id="gridCheck">
                                                         </div>
                                                     </div>
-                                                    <button type="submit" class="btn btn-primary">Sign in</button>
+                                                    <button type="submit" class="btn btn-primary">Submit Order Details</button>
                                                 </form>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -172,8 +188,7 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <?php $i++; ?>
+            <?php $i++; ?>
 
         <?php endforeach; ?>
     </div>
